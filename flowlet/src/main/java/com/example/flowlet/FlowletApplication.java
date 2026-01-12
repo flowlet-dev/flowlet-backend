11446bf3aef34545b349087b21bb5844723fd420
Ito Shun <jacksonis16019207@gmail.com>
2026-01-13 01:04:33 +0900

    `mainメソッドのスコープをstaticに変更`

--- flowlet/src/main/java/com/example/flowlet/FlowletApplication.java
+++ flowlet/src/main/java/com/example/flowlet/FlowletApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlowletApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(FlowletApplication.class, args);
     }
 
